4b9c17757b610bff66d671c8fbd218b3b40a3446
EmanDeveloper <memankhadim@gmail.com>
2026-07-01 15:06:04 +0500

    mongodb setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,16 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
+
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UserModule,
+    MongooseModule.forRoot(process.env.MONGO_URI as string),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
