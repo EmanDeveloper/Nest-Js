4de87b65bbbcda9afb6d11038c2a22e1c1cad284
EmanDeveloper <memankhadim@gmail.com>
2026-07-07 11:33:41 +0500

    user validation

--- src/auth/dto/registerUser.dto.ts
+++ src/auth/dto/registerUser.dto.ts
@@ -1,8 +1,18 @@
 import { UserRole } from "src/user/schemas/user.schema";
-
+import { IsEmail, IsEnum, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 export class RegisterUserDto {
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(2)
+    @MaxLength(100)
     username!: string;
+    @IsEmail()
     email!: string;
+    @IsEnum(UserRole)
     role!: UserRole;
+    @IsNotEmpty()
+    @IsString()
+    @MinLength(6)
+    @MaxLength(100)
     password!: string;
 }
